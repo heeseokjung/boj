baa0f795ef8e1378221ae4800df59f1039000760
heeseok <hycse17@gmail.com>
2019-04-20 10:34:15 -0700

    boj 1065

---
+++ boj_1065.cpp
@@ -0,0 +1,40 @@
+// boj_1065
+
+#include <cstdio>
+
+using namespace std;
+
+int r[3];
+
+bool is_arithmetic_sequence(int x)
+{
+    if(x < 100)
+        return true;
+    else if (x == 1000)
+        return false;
+    
+    int i = 0;
+    while(x) {
+        r[i++] = x % 10;
+        x /= 10;
+    }
+
+    return (r[2] - r[1]) == (r[1] - r[0]);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    
+    int count = 0;
+    for(int i = 1; i <= n; ++i) {
+        if(is_arithmetic_sequence(i))
+            count++;
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
