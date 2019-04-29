b2d14bb2a7b105a0fc7aa3e00489bf45b1687fbb
heeseok <hycse17@gmail.com>
2019-04-29 07:54:36 -0700

    boj 2110 11399

---
+++ boj_11399.cpp
@@ -0,0 +1,33 @@
+// boj_11399
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> time;
+
+int main()
+{
+    int num_people;
+    scanf("%d", &num_people);
+
+    int t;
+    for(int i = 0; i < num_people; ++i) {
+        scanf("%d", &t);
+        time.push_back(t);
+    }
+
+    sort(time.begin(), time.end());
+
+    int current_wait= 0, total_wait = 0;
+    for(int i = 0; i < num_people; ++i) {
+        total_wait = total_wait + current_wait + time[i];
+        current_wait += time[i];
+    }
+
+    printf("%d\n", total_wait);
+    
+    return 0;
+}
---
+++ boj_2110.cpp
@@ -0,0 +1,61 @@
+// boj_2110
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int num_house, num_router;
+vector<int> house;
+
+int run();
+
+int main()
+{
+    scanf("%d %d", &num_house, &num_router);
+    house.reserve(num_house);
+    int location;
+    for(int i = 0 ; i < num_house; ++i) {
+        scanf("%d", &location);
+        house.push_back(location);
+    }
+
+    sort(house.begin(), house.end());
+
+    int max_dist = run();
+
+    printf("%d\n", max_dist);
+    
+    return 0;
+}
+
+int run()
+{
+    int p = 1;
+    int r = house.back() - house.front();
+    int q;
+    int max_dist;
+
+    while(p < r) {
+        q = (p + r) / 2;
+        int count = 1;
+        int prev = 0;
+        for(int i = 0; i < (int)house.size(); ++i) {
+            if(house[i] - house[prev] >= q) {
+                count++;
+                prev = i;
+            }
+        }
+
+        if(count >= num_router) {
+            max_dist = q;
+            p = q + 1;
+        }
+        else {
+            r = q;
+        }
+    }
+
+    return max_dist;
+}
