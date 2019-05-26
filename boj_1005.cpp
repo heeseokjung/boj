a5c7844bb1e3f4f97de1dfd685f98af86431ff2a
2017030273 <2017030273@hanyang.ac.kr>
2019-05-26 04:39:57 -0700

    boj 1005

--- boj_1005.cpp
+++ boj_1005.cpp
@@ -1,25 +1,19 @@
-#include <stdio.h>
+#include <cstdio>
 #include <string.h>
-#include <utility>
 #include <vector>
+#include <queue>
+
+#define INF 1000000000
 
 using namespace std;
 
-typedef struct Building
-{
-    int bid;
-    int time;
-}Building;
-
-typedef pair<int, int> Edge;
-
 int n, k, w;
-Building building[1001];
+int time[1001];
 int indegree[1001] = {0, };
-vector<Edge> edge_list;
-vector<int> ans;
+int answer[1001];
+vector<int> print;
 
-void run();
+void run(vector<int>* adj);
 
 int main()
 {
@@ -28,26 +22,52 @@ int main()
 
     for(int i = 0; i < num_case; ++i) {
         scanf("%d %d", &n, &k);
-        edge_list.reserve(k);
         
-        int time;
-        for(int i = 1; i <= n; ++i) {
-            scanf("%d", &time);
-            building[i].time = time;
-        }
+        for(int i = 1; i <= n; ++i)
+            scanf("%d", &time[i]);
 
         int u, v;
-        memset(indegree + 1, 0, n);
+        vector<int> adj[1001];
+        memset(indegree, 0, sizeof(int) * (n + 1));
         for(int i = 0; i < k; ++i) {
             scanf("%d %d", &u, &v);
-            indegree[u]++;
-            edge_list.push_back(make_pair(v, u));
+            adj[u].push_back(v);
+            indegree[v]++;
         }
 
         scanf("%d", &w);
 
-        run();
+        run(adj);
     }
-    
+
+    for(int i = 0; i < (int)print.size(); ++i)
+        printf("%d\n", print[i]);
+
     return 0;
+}
+
+void run(vector<int>* adj)
+{
+    memset(answer, 0, sizeof(int) * (n + 1));
+    
+    queue<int> q;
+    for(int i = 1; i <= n; ++i) {
+        if(indegree[i] == 0) {
+            answer[i] = time[i];
+            q.push(i);
+        }
+    }
+
+    while(!q.empty()) {
+        int u = q.front(); q.pop();
+        for(int i = 0; i < (int)adj[u].size(); ++i) {
+            int v = adj[u][i];
+            if(answer[v] < answer[u] + time[v])
+                answer[v] = answer[u] + time[v];
+            if(--indegree[v] == 0)
+                q.push(v);
+        }
+    }
+
+    print.push_back(answer[w]);
 }
