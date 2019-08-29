fce49b6418836bd460d8ef1f730ef9f32f8d8fea
heeseok-goormide <hsjeong.cs@gmail.com>
2019-08-29 05:01:18 +0000

    boj 11657

--- boj_11657.cpp
+++ boj_11657.cpp
@@ -1,61 +1,62 @@
-#include <cstdio>
+#include <stdio.h>
 #include <utility>
 #include <vector>
-
+#include <queue>
+#include <algorithm>
 using namespace std;
 
-#define INF 1000000000
-
 int N, M;
+vector<pair<int, int> > adj[501];
+int is_queue[501];
+int cnt[501];
 int dist[501];
-typedef pair<int, int> ii;
-vector<pair<int, ii> > edge_list;
+#define INF 1e9
 
-void run(int src)
-{   
-    dist[src] = 0;
-    bool is_negative_cycle = false;
-    for(int i = 1; i <= N; ++i) {
-        bool is_relaxed = false;
-        for(int j = 0; j < (int)edge_list.size(); ++j) {
-            int u = edge_list[j].first;
-            int v = edge_list[j].second.first;
-            int w = edge_list[j].second.second;
-            if(dist[u] + w < dist[v] && dist[u] != INF) {
-                is_relaxed = true;
-                if(i == N)
-                    is_negative_cycle = true;
-                dist[v] = dist[u] + w;
-            }
-        }
-        if(!is_relaxed)
-            break;
-    }
+void input()
+{
+	scanf("%d %d", &N, &M);
+	int u, v, w;
+	for(int i = 0; i < M; ++i) {
+		scanf("%d %d %d", &u, &v, &w);
+		adj[u].push_back(pair<int, int>(v, w));
+	}
+}
 
-    if(is_negative_cycle)
-        printf("%d\n", -1);
-    else {
-        for(int i = 2; i <= N; ++i) {
-            if(dist[i] == INF)
-                printf("%d\n", -1);
-            else
-                printf("%d\n", dist[i]);
-        }
-    }
+void run()
+{
+	for(int i = 1; i <= N; ++i)
+		dist[i] = INF;
+	
+	dist[1] = 0; is_queue[1] = true; cnt[1]++;
+	queue<int> q;
+	q.push(1);
+	while(!q.empty()) {
+		int u = q.front();
+		q.pop();
+		is_queue[u] = false;
+		for(int i = 0; i < (int)adj[u].size(); ++i) {
+			int v = adj[u][i].first, w = adj[u][i].second;
+			if(dist[v] > dist[u] + w) {
+				dist[v] = dist[u] + w;
+				if(!is_queue[v]) {
+					if(++cnt[v] >= N) {
+						printf("-1\n");
+						return;
+					}
+					q.push(v);
+					is_queue[v] = true;
+				}
+			}
+		}
+	}
+	
+	for(int i = 2; i <= N; ++i)
+		printf("%d\n", (dist[i] == INF) ? -1 : dist[i]);
 }
 
 int main()
 {
-    scanf("%d %d", &N, &M);
-    for(int i = 0; i < M; ++i) {
-        int u, v, w;
-        scanf("%d %d %d", &u, &v, &w);
-        edge_list.push_back(make_pair(u, make_pair(v, w)));
-    }
-    for(int i = 1; i <= N; ++i)
-        dist[i] = INF;
-
-    run(1);
-
-    return 0;
+	input();
+	run();
+	return 0;
 }
