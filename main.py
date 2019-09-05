4ba1df25f8b6512a569d54b2b312ae2deb0722a2
heeseok <hycse17@gmail.com>
2019-09-04 18:03:19 -0700

    2019/09/05

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ while True:
     line = f.readline()
     if not line:
         break
-    os.system('sudo chmod 774 ' + line)
+    os.system('sudo chmod 664 ' + line)
 
 sp.call(['git', 'add', '.'])
-sp.call(['git', 'commit', '-m', 'test'])
+sp.call(['git', 'commit', '-m', '2019/09/05'])
 sp.call(['git', 'push', 'origin', 'master'])
