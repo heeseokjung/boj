6ae419404d78afdfc270cbdcd5955ffbf0d26dfa
heeseok <hycse17@gmail.com>
2019-09-04 17:56:27 -0700

    test

---
+++ main.py
@@ -0,0 +1,7 @@
+import subprocess as sp
+
+sp.call(['git', 'add', '.'])
+sp.call(['git', 'commit', '-m', 'test'])
+sp.call(['git', 'push', 'origin', 'master'])
+sp.call(['heeseokjung'])
+sp.call(['e8652a63'])
