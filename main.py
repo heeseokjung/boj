633c8fe3ebaf8a66d10e7aee925dff73b4fe614c
heeseok <hycse17@gmail.com>
2019-09-04 18:01:42 -0700

    test

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import subprocess as sp
+import os
+
+f = open('file_list.txt', 'r')
+while True:
+    line = f.readline()
+    if not line:
+        break
+    os.system('sudo chmod 774 ' + line)
 
 sp.call(['git', 'add', '.'])
 sp.call(['git', 'commit', '-m', 'test'])
 sp.call(['git', 'push', 'origin', 'master'])
-sp.call(['heeseokjung'])
-sp.call(['e8652a63'])
