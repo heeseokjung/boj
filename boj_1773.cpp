bff60b1762476619f119c45cb386cd08167b8078
heeseok <hycse17@gmail.com>
2020-02-11 17:11:07 -0800

    2020/02/12

--- boj_1773.cpp
+++ boj_1773.cpp
@@ -2,40 +2,23 @@
 #include <vector>
 using namespace std;
 
-inline void swap(int& x, int& y) {
-    int tmp = x;
-    x = y;
-    y = tmp;
-}
-
-int GCD(int a, int b) {
-    if(a < b)
-        swap(a, b);
-    while(a % b) {
-        int tmp = a;
-        a = b;
-        b = tmp % b;
-    }
-    return b;
-}
+int N, C;
+bool check[2000001];
 
 int main() {
-    int N, C;
     scanf("%d %d", &N, &C);
     vector<int> v(N);
     for(int i = 0; i < N; ++i)
         scanf("%d", &v[i]);
-    int gcd = v[0];
-    for(int i = 1; i < N; ++i) 
-        gcd = GCD(gcd, v[i]);
-    long long lcm = 1;
-    for(int i = 0; i < N; ++i)
-        lcm = lcm * (long long)(v[i] / gcd);
-    lcm = lcm * (long long)gcd;
-    long long ans = 0;
-    for(int i = 0; i < N; ++i) 
-        ans += (long long)(C / v[i]);
-    ans -= (((long long)C / lcm) * (long long)(N-1));
-    printf("%lld\n", ans);
+    int count = 0;
+    for(int i = 0; i < N; ++i) {
+        for(int j = 1; v[i]*j <= C; ++j) {
+            if(!check[v[i]*j]) {
+                check[v[i]*j] = true;
+                count++;
+            }
+        }
+    }
+    printf("%d\n", count);
     return 0;
 }
