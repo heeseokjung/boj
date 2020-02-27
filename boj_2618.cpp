81f75e0b05e5d780717335676b5527cd99654efd
heeseok <hycse17@gmail.com>
2020-02-26 21:09:46 -0800

    2020/02/27

--- boj_2618.cpp
+++ boj_2618.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int N, W;
-int dp[1000][2];
-int c[1000];
+int cache[1010][1010];
+int p[1010][1010];
 vector<pair<int, int> > v;
 
 inline int absl(int x) {
@@ -17,41 +17,49 @@ inline int dist(pair<int, int> p1, pair<int, int> p2) {
     return absl(p1.first - p2.first) + absl(p1.second - p2.second);
 }
 
+int dp(int i, int j) {
+    int k = max(i, j) + 1;
+    if(k == W+2)
+        return 0;
+    if(cache[i][j] != -1)
+        return cache[i][j];
+    int x = dp(k, j) + dist(v[i], v[k]);
+    int y = dp(i, k) + dist(v[j], v[k]);
+    if(x < y) {
+        p[i][j] = 1;
+        return cache[i][j] = x;
+    } else {
+        p[i][j] = 2;
+        return cache[i][j] = y;
+    }
+}
+
 int main() {
     scanf("%d %d", &N, &W);
-    v.reserve(W);
+    v.reserve(W+2);
+    v.push_back(pair<int, int>(1, 1));
+    v.push_back(pair<int, int>(N, N));
     for(int i = 0; i < W; ++i) {
         int r, c;
         scanf("%d %d", &r, &c);
         v.push_back(pair<int, int>(r, c));
     }
-    pair<int, int> p[2];
-    p[0] = pair<int, int>(1, 1);
-    p[1] = pair<int, int>(N, N);
-    dp[0][0] = dist(p[0], v[0]);
-    dp[0][1] = dist(p[1], v[0]);
-    if(dp[0][0] < dp[0][1]) {
-        c[0] = 0;
-        p[0] = v[0];
-    } else {
-        c[0] = 1;
-        p[1] = v[0];
-    }
-    for(int i = 1; i < W; ++i) {
-        for(int j = 0; j < 2; ++j) {
-            dp[i][j] = dp[i-1][j] + dist(v[i-1], v[i]);
-            dp[i][j] = min(dp[i][j], dp[i-1][1-j] + dist(p[j], v[i]));
-        }
-        if(dp[i][0] < dp[i][1]) {
-            c[i] = 0;
-            p[0] = v[i];
-        } else {
-            c[i] = 1;
-            p[1] = v[i];
+    for(int i = 0; i < W+2; ++i)
+        for(int j = 0; j < W+2; ++j)
+            cache[i][j] = -1;
+    printf("%d\n", dp(0, 1));
+    for(int i = 0, j = 1; max(i, j) + 1 < W+2; ) {
+        printf("%d\n", p[i][j]);
+        switch(p[i][j]) {
+            case 1:
+                i = max(i, j) + 1;
+                break;
+            case 2:
+                j = max(i, j) + 1;
+                break;
+            default:
+                break;
         }
     }
-    printf("%d\n", dp[W-1][c[W-1]]);
-    for(int i = 0; i < W; ++i)
-        printf("%d\n", c[i]+1);
     return 0;
 }
