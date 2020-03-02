b560f12b6df598b6e73b8e382fd7347bcc744143
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-03-02 06:09:27 -0800

    2020/03/02

--- boj_3090.cpp
+++ boj_3090.cpp
@@ -11,17 +11,42 @@ inline int absl(int x) {
 }
 
 int find(int mx) {
-    int i = 0, j = mx;
+    int s = 0, e = mx;
+    while(s < e) {
+        int m = (s + e)/2;
+        int sum = 0;
+        for(int i = 0; i < N-1; ++i) {
+            if(absl(v[i] - v[i+1]) > m)
+                sum += absl(v[i] - v[i+1]) - m;
+        }
+        if(sum > T)
+            s = m+1;
+        else
+            e = m;
+    }
+    return s;
 }
 
 int main() {
-    scanf("%d", &N);
+    scanf("%d %d", &N, &T);
     v = vector<int>(N);
     for(int i = 0; i < N; ++i)
         scanf("%d", &v[i]);
     int mx = -1;
     for(int i = 0; i < N-1; ++i)
         mx = max(mx, absl(v[i] - v[i+1]));
-    printf("%d\n", find(mx));
+    int x = find(mx);
+    for(int i = 0; i < N-1; ++i) {
+        if(absl(v[i] - v[i+1]) > x) {
+            int diff = absl(v[i] - v[i+1]) - x;
+            if(v[i] < v[i+1])
+                v[i+1] -= diff;
+            else
+                v[i] -= diff;
+        }
+    }
+    for(int i = 0; i < N; ++i)
+        printf("%d ", v[i]);
+    printf("\n");
     return 0;
 }
