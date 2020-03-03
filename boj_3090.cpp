428a4893918f65dc2c39c78d5c186e04edc2ed6f
heeseok <hycse17@gmail.com>
2020-03-02 18:08:15 -0800

    2020/03/03

--- boj_3090.cpp
+++ boj_3090.cpp
@@ -4,49 +4,56 @@
 using namespace std;
 
 int N, T;
-vector<int> v;
+vector<int> v, cp, ans;
 
 inline int absl(int x) {
     return x < 0 ? -x : x;
 }
 
-int find(int mx) {
+void find(int mx) {
     int s = 0, e = mx;
     while(s < e) {
+        for(int i = 0; i < N; ++i)
+            cp[i] = v[i];
         int m = (s + e)/2;
         int sum = 0;
         for(int i = 0; i < N-1; ++i) {
-            if(absl(v[i] - v[i+1]) > m)
-                sum += absl(v[i] - v[i+1]) - m;
+            if(cp[i+1] - cp[i] > m) {
+                int diff = cp[i+1] - cp[i] - m;
+                sum += diff;
+                cp[i+1] -= diff;
+            }
         }
-        if(sum > T)
+        for(int i = N-1; i > 0; --i) {
+            if(cp[i-1] - cp[i] > m) {
+                int diff = cp[i-1] - cp[i] - m;
+                sum += diff;
+                cp[i-1] -= diff;
+            }
+        }
+        if(sum > T) {
             s = m+1;
-        else
+        } else {
             e = m;
+            for(int i = 0; i < N; ++i)
+                ans[i] = cp[i];
+        }
     }
-    return s;
 }
 
 int main() {
     scanf("%d %d", &N, &T);
     v = vector<int>(N);
+    cp = vector<int>(N);
+    ans = vector<int>(N);
     for(int i = 0; i < N; ++i)
         scanf("%d", &v[i]);
     int mx = -1;
     for(int i = 0; i < N-1; ++i)
         mx = max(mx, absl(v[i] - v[i+1]));
-    int x = find(mx);
-    for(int i = 0; i < N-1; ++i) {
-        if(absl(v[i] - v[i+1]) > x) {
-            int diff = absl(v[i] - v[i+1]) - x;
-            if(v[i] < v[i+1])
-                v[i+1] -= diff;
-            else
-                v[i] -= diff;
-        }
-    }
+    find(mx);
     for(int i = 0; i < N; ++i)
-        printf("%d ", v[i]);
+        printf("%d ", ans[i]);
     printf("\n");
     return 0;
 }
