a6dfb0968d3e5e31de79b22e1846613b97deadde
heeseok <hycse17@gmail.com>
2020-03-03 16:50:45 -0800

    2020/03/04

---
+++ boj_3053.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <math.h>
+#define _USE_MATH_DEFINES
+
+int main() {
+    double r;
+    scanf("%lf", &r);
+    printf("%f\n", r * r * M_PI);
+    printf("%f\n", r * r * 2);
+    return 0;
+}
