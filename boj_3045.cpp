bf9597d1ad3fb3ac9d3c62b0341e6ac6f1a1dc0f
heeseok <hycse17@gmail.com>
2020-03-13 01:28:19 -0700

    2020/03/13

---
+++ boj_3045.cpp
@@ -0,0 +1,161 @@
+#include <cstdio>
+#include <cstdlib>
+#include <utility>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef struct Node {
+    int id;
+    struct Node* prev;
+    struct Node* next;
+}Node;
+
+int N, M;
+vector<pair<char, pair<int, int> > > cmd;
+Node* head;
+Node* pos[500001];
+int dp[500001];
+int past[500001];
+bool valid[500001];
+#define inf 1000000
+
+void init() {
+    head = (Node*)malloc(sizeof(Node));
+    head->id = 1;
+    pos[1] = head;
+    Node* p = head;
+    for(int i = 2; i <= N; ++i) {
+        Node* tmp = (Node*)malloc(sizeof(Node));
+        tmp->id = i;
+        pos[i] = tmp;
+        p->next = tmp;
+        tmp->prev = p;
+        p = tmp;
+    }
+    p->next = head;
+    head->prev = p;
+}
+
+Node* remove(Node* p) {
+    if(p == head)
+        head = p->next;
+    p->next->prev = p->prev;
+    p->prev->next = p->next;
+    return p;
+}
+
+void insert_front(Node* x, Node* y) {
+    if(y == head)
+        head = x;
+    x->next = y;
+    x->prev = y->prev;
+    y->prev = x;
+    x->prev->next = x;
+}
+
+void insert_back(Node* x, Node* y) {
+    x->next = y->next;
+    x->prev = y;
+    y->next = x;
+    x->next->prev = x;
+}
+
+void printAll() {
+    Node* p = head;
+    for(int i = 0; i < N; ++i) {
+        printf("%d ", p->id);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+void runcmd() {
+    for(int i = 0; i < M; ++i) {
+        char c = cmd[i].first;
+        int x = cmd[i].second.first;
+        int y = cmd[i].second.second;
+        //printf("<%c x: %d y: %d> ", c, x, y);
+        switch(c) {
+            case 'A': {
+                Node* p = remove(pos[x]);
+                insert_front(p, pos[y]);
+                //printAll();
+                break;
+            }
+            case 'B': {
+                Node* p = remove(pos[x]);
+                insert_back(p, pos[y]); 
+                //printAll();
+                break;
+            }
+            default: 
+                break;
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &N, &M);
+    cmd.reserve(M);
+    getchar();
+    for(int i = 0; i < M; ++i) {
+        char c;
+        int x, y;
+        scanf("%c %d %d", &c, &x, &y);
+        cmd.push_back(make_pair(c, pair<int, int>(x, y)));
+        getchar();
+    }
+
+    init();
+    runcmd();
+
+    dp[0] = 0;
+    for(int i = 1; i <= N; ++i)
+        dp[i] = inf;
+    
+    int mx = 0, k;
+    Node* p = head;
+    for(int i = 0; i < N; ++i) {
+        //printf("x: %d\n", p->id);
+        int d = (int)(upper_bound(dp, dp + N, p->id) - dp);
+        dp[d] = p->id;
+        past[p->id] = dp[d-1];
+        mx = (mx < d) ? d : mx;
+        p = p->next;
+    }
+    k = N - mx;
+
+    vector<int> v;
+    int x = dp[mx];
+    //printf("x: %d past[x]: %d\n", x, past[x]);
+    while(x) {
+        //printf("x: %d\n", x);
+        valid[x] = true;
+        v.push_back(x);
+        x = past[x];
+    }
+    reverse(v.begin(), v.end());
+    /*
+    for(int i = 0; i < (int)v.size(); ++i)
+        printf("%d ", v[i]);
+    printf("\n");
+    */
+
+    printf("%d\n", k);
+    int i = 0, j = 1;
+	while (i < mx) {
+		if (j == v[i]) 
+            ++i;
+		else 
+            printf("A %d %d\n", j, v[i]);
+		++j;
+	}
+    int end = v.back();
+    while(j <= N) {
+        printf("B %d %d\n", j, end);
+        end = j;
+        ++j;
+    }
+    return 0;
+}
