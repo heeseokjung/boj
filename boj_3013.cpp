107087cdc4bd6fe7e64f7ae05387fa4ffba84714
heeseok <hycse17@gmail.com>
2020-03-30 01:21:15 -0700

    2020/03/30

--- boj_3013.cpp
+++ boj_3013.cpp
@@ -19,5 +19,10 @@ int main() {
             break;
         }
     }
+
+    // LET delat(x): 수열 x의 B보다 큰원소의 갯수 - B보다 작은 원소의 갯수
+    // B로 끝나는 모든 부분수열의 delta값을 left배열에 저장
+    // B에서 시작하는 모든 부분수열의 delta값을 right배열에 저장
+    // [-N, N]에 대해 
     return 0;
 }
