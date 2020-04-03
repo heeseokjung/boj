11e80457c50c49d984f23a44fd4f663622476fd9
heeseok <hycse17@gmail.com>
2020-04-02 22:32:27 -0700

    2020/04/03

--- boj_3006.cpp
+++ boj_3006.cpp
@@ -5,9 +5,36 @@ using namespace std;
 int N;
 vector<int> v;
 int pos[100001];
+int tree[2000000];
 
-inline int absl(int x) {
-    return x < 0 ? -x : x;
+int init_tree(int i, int j, int n) {
+    if(i == j) {
+        return tree[n] = 1;
+    } else {
+        int left = init_tree(i, (i + j)/2, n*2);
+        int right = init_tree((i+j)/2 + 1, j, n*2 + 1);
+        return tree[n] = left + right;
+    }
+}
+
+int query(int i, int j, int l, int r, int n)
+{
+    if(i > r || j < l)
+        return 0;
+    if(l <= i && j <= r)
+        return tree[n];
+    return query(i, (i+j)/2, l, r, n*2) + query((i+j)/2 + 1, j, l, r, n*2 + 1);
+}
+
+void update_sum(int i, int j, int n, int idx, int diff)
+{   
+    if(idx < i || idx > j)
+        return;
+    tree[n] += diff;
+    if(i != j) {
+        update_sum(i, (i+j)/2, n*2, idx, diff);
+        update_sum((i+j)/2 + 1, j, n*2 + 1, idx, diff);
+    }
 }
 
 int main() {
@@ -17,12 +44,18 @@ int main() {
         scanf("%d", &v[i]);
     for(int i = 0; i < N; ++i)
         pos[v[i]] = i;
+    
+    init_tree(0, N-1, 1);
     int i, j;
     for(i = 1, j = N; i < j; ++i, --j) {
-        printf("%d\n", absl((i-1) - pos[i]));
-        printf("%d\n", absl((j-1) - pos[j]));
+        printf("%d\n", query(0, N-1, 0, pos[i], 1) - 1);
+        update_sum(0, N-1, 1, pos[i], -1);
+        printf("%d\n", query(0, N-1, pos[j], N-1, 1) - 1);
+        update_sum(0, N-1, 1, pos[j], -1);
     }
-    if(N % 2)
-        printf("%d\n", absl((i-1) - pos[i]));
+    if(N % 2) {
+        printf("%d\n", query(0, N-1, pos[i], N-1, 1) - 1);
+        update_sum(0, N-1, 1, pos[i], -1);
+    }
     return 0;
 }
