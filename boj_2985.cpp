72868f8a965d056fdfc162fec9eda1ca591cf543
heeseok <hycse17@gmail.com>
2020-04-08 17:54:20 -0700

    2020/04/09

---
+++ boj_2985.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+int main() {
+    int x, y, z;
+    scanf("%d %d %d", &x, &y, &z);
+    if(x + y == z)
+        printf("%d+%d=%d\n", x, y, z);
+    else if(x == y + z)
+        printf("%d=%d+%d\n", x, y, z);
+    else if(x - y == z)
+        printf("%d-%d=%d\n", x, y, z);
+    else if(x == y - z)
+        printf("%d=%d-%d\n", x, y, z);
+    else if(x * y == z)
+        printf("%d*%d=%d\n", x, y, z);
+    else if(x == y * z)
+        printf("%d=%d*%d\n", x, y, z);
+    else if(x / y == z)
+        printf("%d/%d=%d\n", x, y, z);
+    else if(x == y / z)
+        printf("%d=%d/%d\n", x, y, z);
+    return 0;
+}
