436d2a164a875f5c5ea7f3b697e6bff261d90f18
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-04-15 03:39:12 -0700

    2020/04/15

--- boj_2982.cpp
+++ boj_2982.cpp
@@ -20,8 +20,8 @@ void run() {
     typedef pair<int, int> ii;
     priority_queue<ii, vector<ii>, greater<ii> > pq;
 
-    dist[A] = 0;
-    pq.push(pair<int, int>(0, A));
+    dist[A] = K;
+    pq.push(pair<int, int>(K, A));
     while(!pq.empty()) {
         int u = pq.top().second;
         int d = pq.top().first;
@@ -32,10 +32,11 @@ void run() {
             if(u == v || adj[u][v] == 0)
                 continue;
             int est = d + adj[u][v];
-            if(ban[u][v] && d < arrive[u] + adj[u][v])
+            printf("u: %d v: %d d: %d arrive[u]: %d adj[u][v]: %d\n", u, v, d, arrive[u], adj[u][v]);
+            if(ban[u][v] && d >= arrive[u] && d < arrive[u] + adj[u][v]) {
                 est += arrive[u] + adj[u][v] - d;
+            }
             if(dist[v] > est) {
-                printf("v: %d\n", v);
                 dist[v] = est;
                 pq.push(pair<int, int>(dist[v], v));
             }
@@ -55,21 +56,20 @@ int main() {
         adj[u][v] = w;
         adj[v][u] = w;
     }
-    
-    int time = K;
-    for(int i = 0; i < G; ++i) {
+
+    for(int i = 0, j = 0; i < G; ++i) {
         int u = path[i];
-        arrive[u] = time;
+        arrive[u] = j;
         if(i < G-1) {
             int v = path[i+1];
             ban[u][v] = true;
-            time += adj[u][v];
+            j += adj[u][v];
         }
     }
 
     run();
 
-    printf("%d\n", dist[B]);
+    printf("%d\n", dist[B] - K);
 
     for(int i = 1; i <= N; ++i)
         printf("dist[%d]: %d arrive[%d]: %d\n", i, dist[i], i, arrive[i]);
