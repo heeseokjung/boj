1c593934c296249c380746777bd2e7167e87abe1
heeseokjung <hsjeong.cs@gmail.com>
2020-04-17 00:49:44 -0700

    2020/04/17

--- boj_2983.cpp
+++ boj_2983.cpp
@@ -2,38 +2,22 @@
 #include <string>
 #include <utility>
 #include <vector>
-#include <algorithm>
+#include <tuple>
+#include <set>
 using namespace std;
 
 int N, K;
 string cmd;
 vector<pair<int, int> > v;
-
-typedef struct Point {
-    int key;
-    int x;
-    int y;
-    int idx;
-}Point;
-
-vector<struct Point> sum;
-vector<struct Point> diff;
-int sum_idx[100001];
-int diff_idx[100001];
-bool visit[100001];
+set<tuple<int, int, int> > sum;
+set<tuple<int, int, int> > diff;
 
 inline int absl(int x) {
     return x < 0 ? -x : x;
 }
 
-inline bool comp(Point& p1, Point& p2) {
-    if(p1.key < p2.key) {
-        return true;
-    } else if(p1.key > p2.key) {
-        return false;
-    } else {
-        return p1.x < p2.x;
-    }
+inline bool same_line(pair<int, int>& p1, pair<int, int>& p2) {
+    return absl(p1.first - p2.first) == absl(p1.second - p2.second);
 }
 
 int main() {
@@ -48,70 +32,63 @@ int main() {
         v.push_back(pair<int, int>(x, y));
     }
 
-    sum.reserve(N);
-    diff.reserve(N);
     for(int i = 0; i < N; ++i) {
-        sum.push_back(Point{v[i].first + v[i].second, v[i].first, v[i].second, i});
-        diff.push_back(Point{v[i].first - v[i].second, v[i].first, v[i].second, i});
-    }
-
-    sort(sum.begin(), sum.end(), comp);
-    sort(diff.begin(), diff.end(), comp);
-
-    for(int i = 0; i < N; ++i) {
-        sum_idx[sum[i].idx] = i;
-        diff_idx[diff[i].idx] = i;
+        sum.insert(make_tuple(v[i].first + v[i].second, v[i].first, i));
+        diff.insert(make_tuple(v[i].first - v[i].second, v[i].first, i));
     }
 
     int pos = 0;
     for(int i = 0; i < K; ++i) {
-        visit[pos] = true;
         switch(cmd[i]) {
             case 'A': {
-                int p = diff_idx[pos];
-                int q = p + 1;
-                while(q < N && visit[diff[q].idx])
-                    q++;
-                if(q < N && diff[p].key == diff[q].key && 
-                   absl(diff[p].x - diff[q].x) == absl(diff[p].y - diff[q].y)) {
-                    //change = true;
-                    pos = diff[q].idx;
+                auto it = diff.find(make_tuple(v[pos].first - v[pos].second, v[pos].first, pos));
+                if(++it == diff.end())
+                    break;
+                int key, x, idx;
+                tie(key, x, idx) = *it;
+                if(v[pos].first - v[pos].second == key) {
+                    sum.erase(make_tuple(v[pos].first + v[pos].second, v[pos].first, pos));
+                    diff.erase(prev(it));
+                    pos = idx;
                 }
                 break;
             }
             case 'B': {
-                int p = sum_idx[pos];
-                int q = p + 1;
-                while(q < N && visit[sum[q].idx])
-                    q++;
-                if(q < N && sum[p].key == sum[q].key &&
-                   absl(sum[p].x - sum[q].x) == absl(sum[p].y - sum[q].y)) {
-                    //change = true;
-                    pos = sum[q].idx;
+                auto it = sum.find(make_tuple(v[pos].first + v[pos].second, v[pos].first, pos));
+                if(++it == sum.end())
+                    break;
+                int key, x, idx;
+                tie(key, x, idx) = *it;
+                if(v[pos].first + v[pos].second == key) {
+                    sum.erase(prev(it));
+                    diff.erase(make_tuple(v[pos].first - v[pos].second, v[pos].first, pos));
+                    pos = idx;
                 }
                 break;
             }
             case 'C': {
-                int p = sum_idx[pos];
-                int q = p - 1;
-                while(q >= 0 && visit[sum[q].idx])
-                    q--;
-                if(q >= 0 && sum[p].key == sum[q].key && 
-                   absl(sum[p].x - sum[q].x) == absl(sum[p].y - sum[q].y)) {
-                    //change = true;
-                    pos = sum[q].idx;
+                auto it = sum.find(make_tuple(v[pos].first + v[pos].second, v[pos].first, pos));
+                if(--it == prev(sum.begin()))
+                    break;
+                int key, x, idx;
+                tie(key, x, idx) = *it;
+                if(v[pos].first + v[pos].second == key) {
+                    sum.erase(next(it));
+                    diff.erase(make_tuple(v[pos].first - v[pos].second, v[pos].first, pos));
+                    pos = idx;
                 }
                 break;
             }
             case 'D': {
-                int p = diff_idx[pos];
-                int q = p - 1;
-                while(q >= 0 && visit[diff[q].idx])
-                    q--;
-                if(q >= 0 && diff[p].key == diff[q].key &&
-                   absl(diff[p].x - diff[q].x) == absl(diff[p].y - diff[q].y)) {
-                    //change = true;
-                    pos = diff[q].idx;
+                auto it = diff.find(make_tuple(v[pos].first - v[pos].second, v[pos].first, pos));
+                if(--it == prev(diff.begin()))
+                    break;
+                int key, x, idx;
+                tie(key, x, idx) = *it;
+                if(v[pos].first - v[pos].second == key) {
+                    sum.erase(make_tuple(v[pos].first + v[pos].second, v[pos].first, pos));
+                    diff.erase(next(it));
+                    pos = idx;
                 }
                 break;
             }
