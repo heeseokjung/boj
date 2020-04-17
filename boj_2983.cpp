6936f28f2c829f91bae52612b782602a5d62dd6c
heeseokjung <hsjeong.cs@gmail.com>
2020-04-16 22:43:39 -0700

    2020/04/17

--- boj_2983.cpp
+++ boj_2983.cpp
@@ -3,7 +3,6 @@
 #include <utility>
 #include <vector>
 #include <algorithm>
-#include <cstdio>
 using namespace std;
 
 int N, K;
@@ -38,8 +37,8 @@ inline bool comp(Point& p1, Point& p2) {
 }
 
 int main() {
-    //ios_base::sync_with_stdio(false);
-    //cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> N >> K;
     cin >> cmd;
     v.reserve(N);
@@ -66,81 +65,52 @@ int main() {
 
     int pos = 0;
     for(int i = 0; i < K; ++i) {
-        bool change = false;
         visit[pos] = true;
         switch(cmd[i]) {
             case 'A': {
                 int p = diff_idx[pos];
-                /*
-                printf("p: %d p+1: %d diff[p]-<%d %d> diff[p+1]-<%d %d>\n", p, p+1, diff[p].x, diff[p].y, diff[p+1].x, diff[p+1].y);
-                if(p < N-1 && !visit[diff[p+1].idx] && 
-                   absl(diff[p].x - diff[p+1].x) == absl(diff[p].y - diff[p+1].y)) {
-                    change = true;
-                    pos = diff[p+1].idx;
-                }
-                */
                 int q = p + 1;
                 while(q < N && visit[diff[q].idx])
                     q++;
-                if(q < N && absl(diff[p].x - diff[q].x) == absl(diff[p].y - diff[q].y)) {
-                    change = true;
+                if(q < N && diff[p].key == diff[q].key && 
+                   absl(diff[p].x - diff[q].x) == absl(diff[p].y - diff[q].y)) {
+                    //change = true;
                     pos = diff[q].idx;
                 }
                 break;
             }
             case 'B': {
                 int p = sum_idx[pos];
-                /*
-                printf("p: %d p+1: %d sum[p]-<%d %d> sum[p+1]-<%d %d>\n", p, p+1, sum[p].x, sum[p].y, sum[p+1].x, sum[p+1].y);
-                if(p < N-1 && !visit[sum[p+1].idx] && 
-                   absl(sum[p].x - sum[p+1].x) == absl(sum[p].y - sum[p+1].y)) {
-                    change = true;
-                    pos = sum[p+1].idx;
-                }
-                */
                 int q = p + 1;
                 while(q < N && visit[sum[q].idx])
                     q++;
-                if(q < N && absl(sum[p].x - sum[q].x) == absl(sum[p].y - sum[q].y)) {
-                    change = true;
+                if(q < N && sum[p].key == sum[q].key &&
+                   absl(sum[p].x - sum[q].x) == absl(sum[p].y - sum[q].y)) {
+                    //change = true;
                     pos = sum[q].idx;
                 }
                 break;
             }
             case 'C': {
                 int p = sum_idx[pos];
-                /*
-                printf("p: %d p-1: %d sum[p]-<%d %d> sum[p-1]-<%d %d>\n", p, p-1, sum[p].x, sum[p].y, sum[p-1].x, sum[p-1].y);
-                if(p > 0 && !visit[sum[p-1].idx] && 
-                   absl(sum[p].x - sum[p-1].x) == absl(sum[p].y - sum[p-1].y)) {
-                    change = true;
-                    pos = sum[p-1].idx;
-                }
-                */
                 int q = p - 1;
                 while(q >= 0 && visit[sum[q].idx])
                     q--;
-                if(q >= 0 && absl(sum[p].x - sum[q].x) == absl(sum[p].y - sum[q].y)) {
-                    change = true;
+                if(q >= 0 && sum[p].key == sum[q].key && 
+                   absl(sum[p].x - sum[q].x) == absl(sum[p].y - sum[q].y)) {
+                    //change = true;
                     pos = sum[q].idx;
                 }
                 break;
             }
             case 'D': {
                 int p = diff_idx[pos];
-                /*
-                printf("p: %d p-1: %d diff[p]-<%d %d> diff[p-1]-<%d %d>\n", p, p-1, diff[p].x, diff[p].y, diff[p-1].x, diff[p-1].y);
-                if(p > 0 && !visit[diff[p-1].idx] && 
-                   absl(diff[p].x - diff[p-1].x) == absl(diff[p].y - diff[p-1].y)) {
-                    change = true;
-                    pos = diff[p-1].idx;
-                }
-                */
                 int q = p - 1;
                 while(q >= 0 && visit[diff[q].idx])
                     q--;
-                if(q >= 0 && absl(diff[p].x - diff[q].x) == absl(diff[p].y - diff[q].y)) {
-                    change = true;
+                if(q >= 0 && diff[p].key == diff[q].key &&
+                   absl(diff[p].x - diff[q].x) == absl(diff[p].y - diff[q].y)) {
+                    //change = true;
                     pos = diff[q].idx;
                 }
                 break;
@@ -149,8 +119,6 @@ int main() {
                 break;
             }
         }
-        if(!change)
-            break;
     }
 
     cout << v[pos].first << " " << v[pos].second << "\n";
