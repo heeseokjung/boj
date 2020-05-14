48699f633fab5fa42050624617b829e9cbc72494
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-05-14 05:25:32 -0700

    2020/05/14

--- boj_2944.cpp
+++ boj_2944.cpp
@@ -7,8 +7,8 @@ using namespace std;
 int N, H;
 vector<int> adj[20];
 int visit[1050000];
-int count[1050000];
-int num[20];
+long long sum[1050000];
+int fan_out[20];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -18,67 +18,51 @@ int main() {
     for(int i = 0; i < N; ++i) {
         string s;
         cin >> s;
-        for(int j = 0; j < (int)s.size(); ++j) {
+        for(int j = 0; j < N; ++j) {
             if(s[j] == '1')
                 adj[i].push_back(j);
         }
+        fan_out[i] = (int)adj[i].size();
     }
 
     memset(visit, -1, sizeof(visit));
 
-    long long sum = 0;
-    int state = 1, step = 0;
-    visit[state] = step++;
-    num[0] = 1;
-    vector<int> cycle;
-    while(H) {
-        int cnt = 0;
-        int next[20];
-        for(int i = 0; i < N; ++i)
-            next[i] = 0;
-        for(int i = 0; i < N; ++i) {
-            if(num[i] % 2) {
-                cnt += (int)adj[i].size();
-                for(int j = 0; j < (int)adj[i].size(); ++j)
-                    next[adj[i][j]]++;
+    int state = 0;
+    for(int i = 0; i < (int)adj[0].size(); ++i)
+        state |= (1<<adj[0][i]);
+    visit[state] = 1;
+    sum[1] = fan_out[0];
+    
+    long long ans = sum[1];
+    for(int i = 2; i <= H; ++i) {
+        visit[state] = i;
+        int next_state = 0, count = 0;
+        for(int j = 0; j < N; ++j) {
+            if(state & (1<<j)) {
+                for(int k = 0; k < (int)adj[j].size(); ++k)
+                    next_state ^= (1<<adj[j][k]);
+                count += fan_out[j];
             } else {
-                cnt += (int)adj[i].size() * 2;
-                for(int j = 0; j < (int)adj[i].size(); ++j)
-                    next[adj[i][j]] += 2;
+                count += 2*fan_out[j];
             }
         }
-        count[step] = cnt;
-        --H;
 
-        int next_state = 0;
-        for(int i = 0; i < N; ++i) {
-            num[i] = next[i];
-            if(num[i] % 2)
-                next_state |= (1 << i);
-        }
+        ans += (long long)count;
+        sum[i] = sum[i-1] + (long long)count;
 
         if(visit[next_state] != -1) {
-            for(int i = visit[next_state]; i <= visit[state]; ++i)
-                cycle.push_back(count[i]);
-            break;
+            int s = visit[next_state], e = i;
+            int rep = (H - i)/(e - s + 1);
+            int remain = (H - i)%(e - s + 1);
+            ans += (long long)rep * (sum[e] - sum[s-1]);
+            ans += (sum[s+remain-1] - sum[s-1]);
+            break;  
         } else {
-            visit[next_state] = ++step;
             state = next_state;
         }
     }
 
-    if(H) {
-        int cycle_size = (int)cycle.size();
-        int cycle_sum = 0;
-        for(int i = 0; i < cycle_size; ++i)
-            cycle_sum += cycle[i];
-        sum += (long long)cycle_sum * (long long)(H / cycle_size);
-        for(int i = 0; i < (H % cycle_size); ++i)
-            sum += (long long)cycle[i];
-        cout << sum << "\n";
-    } else {
-        cout << sum << "\n";
-    }
+    cout << ans << "\n";
 
     return 0;
 }
