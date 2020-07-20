9c9ba2706fadac2952b499fb45a59ed9379b495f
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-07-20 07:38:34 -0700

    2020/07/20

--- boj_1038.cpp
+++ boj_1038.cpp
@@ -4,15 +4,27 @@
 int main() {
     int N;
     scanf("%d", &N);
-    long long dp[10][10];
-    /*
-     * i: number of most significant digit
-     * j: # total digit
-    */
+    int dp[10][11];
     memset(dp, 0, sizeof(dp));
     
-    for(int i = 1; i <= 9; ++i)
+    for(int i = 0; i < 10; ++i)
         dp[i][1] = 1;
+    for(int j = 2; j < 11; ++j) {
+        dp[1][j] = 1;
+        for(int i = 2; i < 10; ++i) {
+            for(int k = 1; k < i; ++k) {
+                dp[i][j] += dp[k][j-1];
+            }
+        }
+    }
+
+    N++;
+    int count = 0;
+    for(int j = 1; j < 11; ++j) {
+        for(int i = 0; i < 10; ++i) {
+            if(count + dp[i][j] < )
+        }
+    }
     
     return 0;
 }
