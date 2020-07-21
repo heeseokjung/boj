6bb954fcac6189a230cf0bf206ac3649efa854f3
heeseokjung <hsjeong.cs@gmail.com>
2020-07-21 00:49:30 -0700

    2020/07/21

--- boj_1038.cpp
+++ boj_1038.cpp
@@ -1,30 +1,34 @@
 #include <cstdio>
-#include <cstring>
+#include <vector>
+#include <queue>
+using namespace std;
 
 int main() {
     int N;
     scanf("%d", &N);
-    int dp[10][11];
-    memset(dp, 0, sizeof(dp));
-    
-    for(int i = 0; i < 10; ++i)
-        dp[i][1] = 1;
-    for(int j = 2; j < 11; ++j) {
-        dp[1][j] = 1;
-        for(int i = 2; i < 10; ++i) {
-            for(int k = 1; k < i; ++k) {
-                dp[i][j] += dp[k][j-1];
-            }
-        }
+    if(N >= 1023) {
+        printf("-1\n");
+        return 0;
     }
 
-    N++;
-    int count = 0;
-    for(int j = 1; j < 11; ++j) {
-        for(int i = 0; i < 10; ++i) {
-            if(count + dp[i][j] < )
+    vector<long long> v(1023);
+    queue<long long> q;
+    int idx;
+    for(idx = 1; idx < 10; ++idx) {
+        q.push((long long)idx);
+        v[idx] = idx;
+    }
+
+    while(!q.empty() && idx <= N) {
+        long long num = q.front(); q.pop();
+        int lsd = num % 10LL;
+        for(int i = 0; i < lsd; ++i) {
+            q.push(num*10LL + (long long)i);
+            v[idx++] = num*10LL + (long long)i;
         }
     }
     
+    printf("%lld\n", v[N]);
+     
     return 0;
 }
