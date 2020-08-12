da8126a3e5faf2bb1344c391a32f0201473a649a
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-08-12 11:22:29 -0700

    2020/08/13

--- boj_2638.cpp
+++ boj_2638.cpp
@@ -6,9 +6,31 @@ using namespace std;
 int N, M;
 int ans = 0;
 int arr[100][100];
+bool air[100][100];
 bool visit[100][100];
 int dr[4] = {-1, 1, 0, 0};
 int dc[4] = {0, 0, -1, 1};
+
+void find_air(pair<int, int> p) {
+    queue<pair<int, int> > q;
+    air[p.first][p.second] = true;
+    q.push(p);
+    while(!q.empty()) {
+        int r = q.front().first;
+        int c = q.front().second;
+        q.pop();
+        for(int i = 0; i < 4; ++i) {
+            int rr = r + dr[i];
+            int cc = c + dc[i];
+            if(rr < 0 || rr > N-1 || cc < 0 || cc > M-1)
+                continue;
+            if(!air[rr][cc] && arr[rr][cc] == 0) {
+                air[rr][cc] = true;
+                q.push({rr, cc});
+            }
+        }
+    }
+}
 
 int main() {
     scanf("%d %d", &N, &M);
@@ -16,16 +38,20 @@ int main() {
         for(int j = 0; j < M; ++j)
             scanf("%d", &arr[i][j]);
 
-    queue<pair<pair<int, int>, int > > q;
-    
+    find_air({0, 0});
+
+    queue<pair<pair<int, int>, int> > q;
+
     for(int i = 0; i < N; ++i) {
         for(int j = 0; j < M; ++j) {
             if(arr[i][j]) {
-                int r, c, count = 0;
+                int count = 0;
                 for(int k = 0; k < 4; ++k) {
-                    r = i + dr[k];
-                    c = j + dc[k];
-                    if(r < 0 || r > N-1 || c < 0 || c > M-1 || arr[r][c] == 0)
+                    int ii = i + dr[k];
+                    int jj = j + dc[k];
+                    if(ii < 0 || ii > N-1 || jj < 0 || jj > M-1)
+                        continue;
+                    if(air[ii][jj])
                         count++;
                 }
                 if(count >= 2) {
@@ -43,22 +69,25 @@ int main() {
         q.pop();
 
         arr[p.first][p.second] = 0;
-        int r, c;
-        for(int i = 0; i < 4; ++i) {
-            r = p.first + dr[i];
-            c = p.second + dc[i];
-            if(arr[r][c] && !visit[r][c]) {
-                int rr, cc, count = 0;
-                for(int j = 0; j < 4; ++j) {
-                    rr = r + dr[j];
-                    cc = c + dc[j];
-                    if(rr < 0 || rr > N-1 || cc < 0 || cc > M-1 || arr[rr][cc] == 0)
-                        count++;
-                }
-                if(count >= 2) {
-                    visit[r][c] = true;
-                    ans = (ans < t+1 ? t+1 : ans);
-                    q.push({{r, c}, t+1});
+        find_air(p);
+
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < M; ++j) {
+                if(!visit[i][j] && arr[i][j]) {
+                    int count = 0;
+                    for(int k = 0; k < 4; ++k) {
+                        int ii = i + dr[k];
+                        int jj = j + dc[k];
+                        if(ii < 0 || ii > N-1 || jj < 0 || jj > M-1)
+                            continue;
+                        if(air[ii][jj])
+                            count++;
+                    }
+                    if(count >= 2) {
+                        visit[i][j] = true;
+                        ans = (ans < t+1 ? t+1 : ans);
+                        q.push({{i, j}, t+1});
+                    }
                 }
             }
         }
