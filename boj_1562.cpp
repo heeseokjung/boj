eb1115bd9ada75b9feb8caef4570635804f4bd9e
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-09-15 07:53:35 -0700

    2020/09/15

--- boj_1562.cpp
+++ boj_1562.cpp
@@ -4,24 +4,29 @@ int N;
 int dp[101][10];
 const int mod = (int)1e9;
 
-inline int abs(int x) {
-    return x < 0 ? -x : x;
-}
-
 int main() {
     scanf("%d", &N);
-    for(int i = 0; i < 10; ++i)
-        dp[1][i] = 1;
-    for(int i = 2; i <= N; ++i) {
+    for(int j = 0; j < 10; ++j)
+        dp[1][j] = 1;
+    for(int j = 1; j < 10; ++j) {
+        if(j-1 >= 0) {
+            dp[2][j-1] += dp[1][j];
+            dp[2][j-1] %= mod;
+        }
+        if(j+1 < 10) {
+            dp[2][j+1] += dp[1][j];
+            dp[2][j+1] %= mod;
+        }
+    }
+    for(int i = 2; i < N; ++i) {
         for(int j = 0; j < 10; ++j) {
-            if(j-1 >= 0)  {
-                if(j-1 == 0 && i == 2)
-                    ;
-                else
-                    dp[i][j] = (dp[i][j] + dp[i-1][j-1])%mod;
+            if(j-1 >= 0) {
+                dp[i+1][j-1] += dp[i][j];
+                dp[i+1][j-1] %= mod;
             }
             if(j+1 < 10) {
-                dp[i][j] = (dp[i][j] + dp[i-1][j+1])%mod;
+                dp[i+1][j+1] += dp[i][j];
+                dp[i+1][j+1] %= mod;
             }
         }
     }
