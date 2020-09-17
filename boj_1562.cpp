b0517b11d92be55d9c96c5ed27b7197b0a7909eb
hsjeong-desktop <hsjeong.cs@gmail.com>
2020-09-17 05:42:54 -0700

    2020/09/17

--- boj_1562.cpp
+++ boj_1562.cpp
@@ -1,42 +1,35 @@
 #include <cstdio>
+#define MOD 1000000000
 
 int N;
-int dp[101][10];
-const int mod = (int)1e9;
+int dp[101][10][2100];
 
 int main() {
     scanf("%d", &N);
-    for(int j = 0; j < 10; ++j)
-        dp[1][j] = 1;
-    for(int j = 1; j < 10; ++j) {
-        if(j-1 >= 0) {
-            dp[2][j-1] += dp[1][j];
-            dp[2][j-1] %= mod;
-        }
-        if(j+1 < 10) {
-            dp[2][j+1] += dp[1][j];
-            dp[2][j+1] %= mod;
-        }
-    }
-    for(int i = 2; i < N; ++i) {
+    
+    for(int i = 1, j = 1; i < 10; ++i)
+        dp[1][i][j<<i] = 1;
+    for(int i = 2; i <= N; ++i) {
         for(int j = 0; j < 10; ++j) {
-            if(j-1 >= 0) {
-                dp[i+1][j-1] += dp[i][j];
-                dp[i+1][j-1] %= mod;
-            }
-            if(j+1 < 10) {
-                dp[i+1][j+1] += dp[i][j];
-                dp[i+1][j+1] %= mod;
+            for(int k = 0; k < (1<<10); ++k) {
+                if(j-1 >= 0)
+                    dp[i][j][k | (1<<j)] += dp[i-1][j-1][k];
+                if(j+1 < 10)
+                    dp[i][j][k | (1<<j)] += dp[i-1][j+1][k];
+                dp[i][j][k] %= MOD;
             }
         }
     }
-    int sum = 0;
-    for(int i = 0; i < 10; ++i)
-        sum = (sum + dp[N][i])%mod;
-    printf("%d\n", sum);
 
-    for(int i = 0; i < 10; ++i)
-        printf("dp[%d][%d]: %d\n", 2, i, dp[2][i]);
+    int sum = 0;
+    for(int i = 0; i < 10; ++i) {
+        for(int j = 0; j < (1<<10); ++j) {
+            if(j == (1<<10)-1)
+                sum += dp[N][i][j];
+            sum %= MOD;
+        }
+    }
+    printf("%d\n", sum);
 
     return 0;
 }
